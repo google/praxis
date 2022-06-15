3bed680df2b8788cef4263988b9b1b7379368939
Laurent El Shafey <shafey@google.com>
2022-06-14 20:01:47 -0700

    Fix docstring.
    PiperOrigin-RevId: 455018233

--- praxis/base_layer.py
+++ praxis/base_layer.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Base class for the Lingvo Flax layers."""
+"""Base class for the Praxis layers."""
 
 from __future__ import annotations
 
