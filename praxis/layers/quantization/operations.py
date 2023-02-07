004eba40182ae6ddbb9d06c237da18d80d87471e
Oleg Rybakov <rybakov@google.com>
2023-02-07 14:30:38 -0800

    Use offset calculation in AQT for backward compatibility with inference service.
    PiperOrigin-RevId: 507886784

--- praxis/layers/quantization/operations.py
+++ praxis/layers/quantization/operations.py
@@ -67,6 +67,47 @@ def _get_min_max(bits):
   return -1 * 2 ** (bits - 1), 2 ** (bits - 1) - 1
 
 
+def compute_offset(x: JTensor, zp: JTensor, eqn: str):
+  """Computes offset: product of activation x with zero point of weight.
+
+  Args:
+    x: Not quantized activation.
+    zp: Not quantized zero point of weight
+    eqn: The equation for the einsum between x and w.
+
+  Returns:
+    Offset tensor
+  """
+
+  def _get_x_reduce_axis(eqn: str, x_dims: int) -> List[int]:
+    """Get reduction axis on activation."""
+    if eqn == 'ANH,DNH->AD' or eqn == 'ABNH,DNH->ABD':
+      return [x_dims - 2, x_dims - 1]
+    else:
+      return [x_dims-1]
+
+  def _get_offset_eqn(eqn: str) -> str:
+    """Gets the offset equation dimensions."""
+    if eqn == 'AD,KDNH->KANH':
+      return 'A,KNH->KANH'
+    if eqn == 'ANH,DNH->AD':
+      return 'A,D->AD'
+    if eqn == '...y,yz->...z':
+      return '...y,z->...yz'
+    if eqn == 'ABD,KDNH->KABNH':
+      return 'AB,KNH->KABNH'
+    if eqn == 'ABNH,DNH->ABD':
+      return 'AB,D->ABD'
+    # Add new equations as needed.
+    raise NotImplementedError(f'eqn {eqn} not supported for asymmetric weight.')
+
+  reduce_axis = _get_x_reduce_axis(eqn, len(x.shape))
+  x_reduce = jnp.sum(x, axis=reduce_axis, keepdims=False)
+  offset_eqn = _get_offset_eqn(eqn)
+  offset = jnp.einsum(offset_eqn, x_reduce, zp)
+  return offset
+
+
 def einsum(
     eqn: str,
     x: JTensor,
@@ -106,35 +147,8 @@ def einsum(
 
   ret = jnp.multiply(ret, scale)
 
-
-  def _get_x_reduce_axis(eqn: str, x_dims: int) -> List[int]:
-    """Get reduction axis on activation."""
-    if eqn == 'ANH,DNH->AD' or eqn == 'ABNH,DNH->ABD':
-      return [x_dims - 2, x_dims - 1]
-    else:
-      return [x_dims-1]
-
-
-  def _get_offset_eqn(eqn: str) -> str:
-    """Gets the offset equation dimensions."""
-    if eqn == 'AD,KDNH->KANH':
-      return 'A,KNH->KANH'
-    if eqn == 'ANH,DNH->AD':
-      return 'A,D->AD'
-    if eqn == '...y,yz->...z':
-      return '...y,z->...yz'
-    if eqn == 'ABD,KDNH->KABNH':
-      return 'AB,KNH->KABNH'
-    if eqn == 'ABNH,DNH->ABD':
-      return 'AB,D->ABD'
-    # Add new equations as needed.
-    raise NotImplementedError(f'eqn {eqn} not supported for asymmetric weight.')
-
   if zp is not None:
-    reduce_axis = _get_x_reduce_axis(eqn, len(x.shape))
-    x_reduce = jnp.sum(x, axis=reduce_axis, keepdims=False)
-    offset_eqn = _get_offset_eqn(eqn)
-    offset = jnp.einsum(offset_eqn, x_reduce, zp)
+    offset = compute_offset(x, zp, eqn)
     ret = ret - offset
   return ret
 
@@ -360,6 +374,8 @@ def dot_general(
     dimension_numbers: lax.DotDimensionNumbers,
     is_eval: bool,
     rhs_quantized: Optional[Tuple[JTensor, JTensor]] = None,
+    rhs_zp: Optional[JTensor] = None,
+    eqn: Optional[str] = None,
 ) -> JTensor:
   """Quantized jax.lax.dot_general.
 
@@ -375,6 +391,8 @@ def dot_general(
     rhs_quantized: A pair of quantized rhs and its scale. It should exist only
       in the inference mode and both rhs and rhs_quantized cannot be passed
       together.
+    rhs_zp: Zero point of right-hand side of the einsum.
+    eqn: The valid binary einsum equation to use.
 
   Returns:
     An array containing the result with the same dtype as 'lhs' and 'rhs'.
@@ -428,7 +446,21 @@ def dot_general(
       lhs_scale, rhs_scale, dimension_numbers=dimension_numbers
   )
 
-  return out * out_scale
+  ret = out * out_scale
+
+  if rhs_zp is not None:
+    if lhs_quantizer.precision is not None:
+      raise NotImplementedError(
+          'Activation quantization with weight zero point is not supported yet.'
+      )
+    if eqn is None:
+      raise NotImplementedError(
+          'eqn has to be specified for zero point calculation.'
+      )
+    offset = compute_offset(lhs, rhs_zp, eqn)
+    ret = ret - offset
+
+  return ret
 
 
 # TODO(b/262309036): Make dot_general the default quantized einsum
@@ -442,6 +474,7 @@ def aqt_einsum(
     rhs_quantizer: aqt.TensorQuantizer,
     is_eval: bool,
     rhs_quantized: Optional[Tuple[JTensor, JTensor]] = None,
+    rhs_zp: Optional[JTensor] = None,
 ) -> JTensor:
   """Quantized einsum using jax.lax.dot_general.
 
@@ -456,6 +489,7 @@ def aqt_einsum(
     rhs_quantized: A pair of quantized rhs and its scale. It should exist only
       in the inference mode and both rhs and rhs_quantized cannot be passed
       together.
+    rhs_zp: Zero point of right-hand side of the einsum.
 
   Returns:
     An array containing the result with the same dtype as 'lhs' and 'rhs'.
@@ -469,6 +503,8 @@ def aqt_einsum(
       dimension_numbers=dimension_numbers,
       is_eval=is_eval,
       rhs_quantized=rhs_quantized,
+      rhs_zp=rhs_zp,
+      eqn=eqn,
   )
   if perm is not None:
     out = lax.transpose(out, perm)
