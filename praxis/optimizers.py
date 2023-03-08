54bdc12c74a92e51c9943b91bccd99505dadaa49
ashors1 <ashors@nvidia.com>
2023-03-08 10:19:48 -0800

    fix formatting, small ga bug fix

--- praxis/optimizers.py
+++ praxis/optimizers.py
@@ -2714,7 +2714,7 @@ class DynamicAccumulator(BaseOptimizer):
 def sharded_static_accumulation(
     num_sub_batches: int,
     clip_gradient_norm_to_value: float,
-    clip_gradient_single_norm_to_value: float, 
+    clip_gradient_single_norm_to_value: float,
     base_tx: ShardedGradientTransformation,
 ) -> ShardedGradientTransformation:
   """Gradient transformation for ShardedStaticAccumulator optimizer."""
@@ -2783,25 +2783,25 @@ def sharded_static_accumulation(
                          lambda: new_count)
 
     def _run_base_tx():
-      
+
       def _compute_grad_norm(grads: NestedMap) -> JTensor:
         """Computes total grad norm."""
         grad_norms_squared = jax.tree_map(lambda x: jnp.sum(x * x), grads)
         grad_norms_squared, _ = jax.tree_util.tree_flatten(grad_norms_squared)
         return jnp.sqrt(jnp.sum(jnp.stack(grad_norms_squared)))
-      
-      
+
+
       def scale_gradients(
           raw_grads: NestedMap,
           clip_grad_norm_to_value: Optional[float] = None,
           clip_grad_single_norm_to_value: Optional[float] = None):
-        
+
         def clip_grads(grads):
           if clip_grad_norm_to_value:
             assert clip_grad_single_norm_to_value == 0.
-            
+
             grad_norm = _compute_grad_norm(raw_grads)
-            
+
             grad_scale = jnp.minimum(
                 jnp.array(1, grad_norm.dtype),
                 jnp.array(clip_grad_norm_to_value, grad_norm.dtype)
@@ -2811,7 +2811,7 @@ def sharded_static_accumulation(
             assert clip_grad_norm_to_value == 0.
             grad_single_norm = jax.tree_map(lambda x: jnp.sqrt(jnp.sum(x * x)),
                                             grads)
-            
+
             def scale_gradient(grad, norm):
               return grad * jnp.minimum(
                   jnp.array(1, norm.dtype),
@@ -2820,17 +2820,17 @@ def sharded_static_accumulation(
             grads = jax.tree_map(scale_gradient, grads, grad_single_norm)
 
           return grads
-        
+
         grads = clip_grads(raw_grads)
         return grads
-    
+
       averaged_updated = jax.tree_map(lambda acc: acc / num_sub_batches,
                                       new_accumulated_update)
-      scaled_updated = scale_gradients(averaged_updated, 
+      scaled_updated = scale_gradients(averaged_updated,
                                        clip_gradient_norm_to_value,
                                        clip_gradient_single_norm_to_value)
       emission_updates, emission_base_state = base_tx.update(
-          averaged_updated, state.base_state, params)
+          scaled_updated, state.base_state, params)
       return (emission_updates,
               jax.tree_map(lambda u: jnp.zeros_like(u, dtype=jnp.float32),
                            updates), emission_base_state)
