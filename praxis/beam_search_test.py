e57439580931d3f70481e5a5f1e8db63f9b5509d
Terry Koo <terrykoo@google.com>
2023-05-15 21:34:46 -0700

    Adds brute-force comparison test for beam_search().
    PiperOrigin-RevId: 532320495

--- praxis/beam_search_test.py
+++ praxis/beam_search_test.py
@@ -15,9 +15,14 @@
 
 """Unit tests for beam_search."""
 
+import heapq
+import math
+import random
+import time
 from typing import Any
 from praxis import pax_fiddle
 
+from absl import logging
 from absl.testing import absltest
 from absl.testing import parameterized
 import jax
@@ -113,10 +118,14 @@ class MockLM(base_layer.BaseLayer):
   """Mock l m.
 
   Attributes:
-    logits: results returned by extend_step(), of shape [max step, batch size,
-      vocab size].
+    stepwise_logits: If True, the logits are keyed by step; otherwise, they are
+      keyed by input token.
+    logits: Premade logits returned by extend_step().  If `stepwise_logits` is
+      True, then `logits[step]` is returned for every batch item.  Otherwise,
+      the `i`th batch has logits `logits[inputs[i]]`.
   """
   logits: Any = None
+  stepwise_logits: bool = False
   model_type: LanguageModelType = LanguageModelType.CAUSAL
 
   def setup(self) -> None:
@@ -135,19 +144,26 @@ class MockLM(base_layer.BaseLayer):
   ) -> Any:
     ret = NestedMap()
     time_step = self.get_variable(DECODE_CACHE, 'time_step')
-    ret.logits = jnp.take(self._logits, inputs, axis=0)
+    if self.stepwise_logits:
+      logits = jnp.take(self._logits, time_step, axis=0)
+      logits = jnp.expand_dims(logits, axis=0)
+      ret.logits = jnp.repeat(logits, inputs.shape[0], axis=0)
+    else:
+      ret.logits = jnp.take(self._logits, inputs, axis=0)
     self.put_variable(DECODE_CACHE, 'time_step', time_step + 1)
     return ret
 
 
 class BeamSearchTest(test_utils.TestCase):
 
-  def _run_decode(self, decoder_p, logits, input_batch):
+  def _run_decode(self, decoder_p, logits, input_batch, stepwise_logits=False):
     p = pax_fiddle.Config(
         models.LanguageModel,
         name='mock_lm',
         decoder_tpl=decoder_p.clone(),
-        lm_tpl=pax_fiddle.Config(MockLM, logits=logits),
+        lm_tpl=pax_fiddle.Config(
+            MockLM, logits=logits, stepwise_logits=stepwise_logits
+        ),
     )
     lang_model = instantiate(p)
     theta = NestedMap(lm_tpl=NestedMap())
@@ -212,6 +228,132 @@ class BeamSearchTest(test_utils.TestCase):
       target_logprobs.append(logprobs_list)
     self.assertArraysEqual(results.logprobs,
                            np.array([target_logprobs], dtype=np.float32))
+
+  def test_beam_search_vs_brute_force_on_random_logits(self):
+    seed = time.time_ns()
+    random.seed(seed)
+    logging.info('PRNG seed: %d', seed)  # for later repro
+
+    batch_size = 1
+    beam_size = 32
+
+    eos_ids = [1, 2]  # exercise multiple end tokens
+    random.shuffle(eos_ids)
+    num_control_tokens = 1 + len(eos_ids)  # <pad> </s1> </s2>
+    num_normal_tokens = 3
+    vocab_size = num_control_tokens + num_normal_tokens
+    prefix_len = 2
+    decoded_len = 5
+    seq_len = prefix_len + decoded_len
+
+    # We use the logs of unique primes as logits, so a score of a decoded path
+    # is a product of unique primes and every path has a provably unique score.
+    # Uniqueness allows the test to avoid tie-breaking and numerical near-ties,
+    # which would make it hard to compare brute force and beam search.
+    primes = [
+        # Since <pad> is not scored, each row is 1 shorter than expected.
+          2,   3,   5,   7,  11,  # 5
+         13,  17,  19,  23,  29,  # 10
+         31,  37,  41,  43,  47,  # 15
+         53,  59,  61,  67,  71,  # 20
+         73,  79,  83,  89,  97,  # 25
+        101, 103, 107, 109, 113,  # 30
+    ]  # pyformat: disable
+    random.shuffle(primes)
+    log_primes = [math.log(prime) for prime in primes]
+    logits = [
+        # Start each row with an infeasible score for <pad>, then slice rows off
+        # of the shuffled list of primes.
+        [-1e20] + log_primes[i * (vocab_size - 1) : (i + 1) * (vocab_size - 1)]
+        for i in range(decoded_len)
+    ]
+    logging.info('logits:\n%s', logits)
+
+    # Pad the logits up to `beam_size`; otherwise, the beam cannot be filled.
+    for row in logits:
+      row.extend([-1e20] * (beam_size - vocab_size))
+    scores = jax.nn.log_softmax(jnp.array(logits, dtype=jnp.float32))
+    logging.info('scores:\n%s', scores[:, :vocab_size])
+
+
+    # Perform brute-force inference to find the true top-k.  Since the logits
+    # are non-contextual (i.e., they do not depend on the previously-decoded
+    # tokens), beam_search() will also find the true top-k and we can compare
+    # results between the two.
+    topk_heap = []  # min-heap of (score, [ids...])
+    for length in range(decoded_len):  # `length` excludes </s>
+      logging.info("enumerating all sequences of length %d", length)
+
+      # Note that beam_search() does not split hypotheses for different choices
+      # of EOS.  That is, while [1, 2, 3] and [1, 2, 4] are distinct hypotheses,
+      # [1, 2, </s1>] and [1, 2, </s2>] are not---beam_search() only tracks the
+      # one with the highest-scoring EOS.  Correspondingly, we pre-maximize the
+      # EOS choices here.
+      best_eos_id = eos_ids[0]
+      for eos_id in eos_ids[1:]:
+        if scores[length, eos_id] > scores[length, best_eos_id]:
+          best_eos_id = eos_id
+
+      # Enumerate all sequences of N tokens from vocabulary V by numbering them
+      # and interpreting each index as an N-digit base-|V| number.
+      for index in range(num_normal_tokens**length):
+        sequence = [0] * prefix_len
+        for _ in range(length):
+          sequence.append(num_control_tokens + index % num_normal_tokens)
+          index //= num_normal_tokens
+        sequence.append(best_eos_id)
+        score = sum([scores[i, t] for i, t in enumerate(sequence[prefix_len:])])
+
+        heapq.heappush(topk_heap, (float(score), sequence))
+        if len(topk_heap) > beam_size:
+          heapq.heappop(topk_heap)
+
+    brute_force_topk = list(reversed(sorted(topk_heap)))
+
+    # Set length_norm_alpha to make length_norm = 1.0
+    length_norm_alpha = 0.0
+    p = models.BeamSearchHParams(
+        beam_size=beam_size,
+        eos_id=eos_ids,
+        fprop_for_prefix=True,
+        max_decode_steps=decoded_len,
+        seqlen=seq_len,
+        length_norm_alpha=length_norm_alpha,
+    )
+    batch_size = 1
+    input_batch = NestedMap(
+        ids=jnp.zeros((batch_size, prefix_len), dtype=jnp.int32),
+        paddings=jnp.zeros((batch_size, prefix_len), dtype=jnp.int32),
+        prefix_lengths=jnp.full((batch_size,), prefix_len, dtype=jnp.int32),
+    )
+    results = self._run_decode(p, logits, input_batch, stepwise_logits=True)
+
+    beam_search_topk = []
+    for k in range(beam_size):
+      score = float(results.scores[0, k])
+      length = results.decode_lengths[0, k]
+      sequence = [int(t) for t in results.output_ids[0, k, :length]]
+      beam_search_topk.append((score, sequence))
+
+    for k, (
+        (brute_force_score, brute_force_sequence),
+        (beam_search_score, beam_search_sequence),
+    ) in enumerate(zip(brute_force_topk, beam_search_topk)):
+      logging.info(
+          'For k=%d\nBrute force: %f %s\nBeam search: %f %s',
+          k,
+          brute_force_score,
+          brute_force_sequence,
+          beam_search_score,
+          beam_search_sequence,
+      )
+
+      self.assertNear(
+          brute_force_score, beam_search_score, 1e-3, f'for hypothesis {k}'
+      )
+      self.assertEqual(
+          brute_force_sequence, beam_search_sequence, f'for hypothesis {k}'
+      )
 
 
 if __name__ == '__main__':
