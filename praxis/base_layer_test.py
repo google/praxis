32b2cefdecc68d5728e0ead5453efea1d215a2af
Laurent El Shafey <shafey@google.com>
2023-06-15 11:19:49 -0700

    Make base_layer_test more robust to Flax version.
    PiperOrigin-RevId: 540637673

--- praxis/base_layer_test.py
+++ praxis/base_layer_test.py
@@ -585,28 +585,32 @@ class BaseLayerTest(test_utils.TestCase):
       self.assertNotIn('parent', cloned.__arguments__)
 
     with self.subTest('to_text'):
-      expected_to_text = (
-          '\n'.join([
-              'activation_split_dims_mapping.out : NoneType',
-              f'cls : type/__main__/{Layer.__qualname__}',
-              'contiguous_submeshes : NoneType',
-              'dcn_mesh_shape : (3, 4)',
-              'dtype : type/jax.numpy/float32',
-              'fprop_dtype : type/jax.numpy/float16',
-              'ici_mesh_shape : (1, 2)',
-              "mesh_axis_names : ('a', 'b')",
-              "name : 'my_layer'",
-              "params_init.method : 'xavier'",
-              'params_init.scale : 1.000001',
-              'shared_weight_layer_id : NoneType',
-              'skip_lp_regularization : NoneType',
-              'weight_split_dims_mapping.wt : NoneType',
-              'x : 3',
-          ])
-          + '\n'
-      )
+      # TODO(pax-team): Preserve only tuple option, once Flax 0.6.11 is
+      # released.
+      expected_to_text_options = []
+      for t in (tuple, list):
+        expected_to_text_options.append(
+            '\n'.join([
+                'activation_split_dims_mapping.out : NoneType',
+                f'cls : type/__main__/{Layer.__qualname__}',
+                'contiguous_submeshes : NoneType',
+                f'dcn_mesh_shape : {t((3, 4))}',
+                'dtype : type/jax.numpy/float32',
+                'fprop_dtype : type/jax.numpy/float16',
+                f'ici_mesh_shape : {t((1, 2))}',
+                f"mesh_axis_names : {t(('a', 'b'))}",
+                "name : 'my_layer'",
+                "params_init.method : 'xavier'",
+                'params_init.scale : 1.000001',
+                'shared_weight_layer_id : NoneType',
+                'skip_lp_regularization : NoneType',
+                'weight_split_dims_mapping.wt : NoneType',
+                'x : 3',
+            ])
+            + '\n'
+        )
       actual_to_text = base_hyperparams.nested_struct_to_text(hparams_stub)
-      self.assertEqual(actual_to_text, expected_to_text)
+      self.assertIn(actual_to_text, expected_to_text_options)
 
     with self.subTest('can_deepcopy'):
       copy.deepcopy(hparams_stub)
