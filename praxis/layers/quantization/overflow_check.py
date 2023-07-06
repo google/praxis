0f119fd0017f7ad20b24e276c2ba22cf6fdb898b
The praxis Authors <no-reply@google.com>
2023-07-06 14:55:05 -0700

    Add overflow checks and logging.
    PiperOrigin-RevId: 546099781

---
+++ praxis/layers/quantization/overflow_check.py
@@ -0,0 +1,151 @@
+# coding=utf-8
+# Copyright 2022 The Pax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Layers with overflow checks."""
+
+import dataclasses
+from typing import Any, Tuple
+from absl import logging
+import jax
+from jax import numpy as jnp
+from praxis import base_layer
+from praxis import pax_fiddle
+from praxis import pytypes
+from praxis.layers import attentions
+from praxis.layers import linears
+
+instance_field = base_layer.instance_field
+JTensor = pytypes.JTensor
+
+
+@dataclasses.dataclass
+class OverflowLimits:
+  """Overflow limits."""
+
+  max_val: float = jnp.finfo(jnp.float16).max
+  min_val: float = jnp.finfo(jnp.float16).min
+
+
+class OverflowChecker(base_layer.BaseLayer):
+  """Adds overflow checks."""
+
+  overflow_limits: OverflowLimits = instance_field(OverflowLimits)
+
+  def check_overflow(self, inputs, class_name, name):
+    max_val = jnp.max(inputs)
+    min_val = jnp.min(inputs)
+    overflow = jnp.logical_or(
+        jnp.greater(max_val, self.overflow_limits.max_val),
+        jnp.less(min_val, self.overflow_limits.min_val),
+    )
+
+    def log_overflow(max_val, min_val):
+      logging.warning(  # pylint: disable=logging-fstring-interpolation
+          f"Overflow in: {class_name}, {name}, max_val: {max_val}, min_val:"
+          f" {min_val}"
+      )
+
+    def _true_fn(max_val, min_val):
+      jax.debug.callback(log_overflow, max_val, min_val)
+
+    def _false_fn(max_val, min_val):
+      del max_val, min_val
+      pass
+
+    jax.lax.cond(overflow, _true_fn, _false_fn, max_val, min_val)
+
+
+class FeedForwardOverflowCheck(linears.FeedForward, OverflowChecker):
+  """Linear layer with overflow checks."""
+
+  def __call__(
+      self,
+      inputs: JTensor,
+  ) -> JTensor:
+    out = super().__call__(inputs)
+    self.check_overflow(out, "FeedForward", self.name)
+    return out
+
+
+class AttentionProjectionOverflowCheck(
+    attentions.AttentionProjection, OverflowChecker
+):
+  """Attention projection layer with overflow checks."""
+
+  def __call__(
+      self,
+      inputs: JTensor,
+  ) -> JTensor:
+    out = super().__call__(inputs)
+    self.check_overflow(out, "AttentionProjection", self.name)
+    return out
+
+  def extend_step(self, inputs: JTensor, *, time_step: JTensor) -> JTensor:
+    del time_step  # Not used.
+    return self.__call__(inputs)
+
+
+class CombinedQKVProjectionLayerOverflowCheck(
+    attentions.CombinedQKVProjectionLayer, OverflowChecker
+):
+  """Combined QKV projection layer with overflow checks."""
+
+  def __call__(
+      self,
+      inputs: JTensor,
+  ) -> Tuple[JTensor, JTensor, JTensor]:
+    q, k, v = super().__call__(inputs)
+    self.check_overflow(q, "CombinedQKVProjection, q", self.name)
+    self.check_overflow(k, "CombinedQKVProjection, k", self.name)
+    self.check_overflow(v, "CombinedQKVProjection, v", self.name)
+    return q, k, v
+
+  def extend_step(self, inputs: JTensor, *, time_step: JTensor) -> JTensor:
+    del time_step  # Not used.
+    return self.__call__(inputs)  # pytype: disable=bad-return-type  # jax-ndarray
+
+
+def add_overflow_checks(
+    task_p: Any,
+    max_val: float = jnp.finfo(jnp.float16).max,
+    min_val: float = jnp.finfo(jnp.float16).min,
+) -> Any:
+  """Adds overflow checks to the task."""
+  overwrite_task_p = task_p.clone()
+
+  transformer_layer_p = (
+      overwrite_task_p.model.lm_tpl.stacked_transformer_tpl.block.transformer_layer_params_tpl
+  )
+
+  overflow_check_tpl = OverflowLimits(max_val=max_val, min_val=min_val)
+  feed_forward = pax_fiddle.Config(
+      FeedForwardOverflowCheck, overflow_limits=overflow_check_tpl
+  )
+  transformer_layer_p.tr_fflayer_tpl.fflayer_tpl = feed_forward
+
+  proj_tpl = pax_fiddle.Config(
+      AttentionProjectionOverflowCheck,
+      overflow_limits=overflow_check_tpl,
+  )
+  transformer_layer_p.tr_atten_tpl.proj_tpl = proj_tpl
+
+  # TODO(chinaev): support other types of attention
+  combined_qkv_proj_tpl = pax_fiddle.Config(
+      CombinedQKVProjectionLayerOverflowCheck,
+      overflow_limits=overflow_check_tpl,
+  )
+  transformer_layer_p.tr_atten_tpl.combined_qkv_proj_tpl = combined_qkv_proj_tpl
+
+  return overwrite_task_p
---
+++ praxis/layers/quantization/overflow_check_test.py
@@ -0,0 +1,172 @@
+# coding=utf-8
+# Copyright 2022 The Pax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for overflow checking."""
+
+from absl.testing import absltest
+import jax
+from jax import numpy as jnp
+import numpy as np
+from praxis import base_layer
+from praxis import layers
+from praxis import pax_fiddle
+from praxis import test_utils
+from praxis.layers import quantization
+from praxis.layers.quantization import overflow_check
+
+instantiate = base_layer.instantiate
+
+
+class CheckOverflowTest(test_utils.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    np.random.seed(123456)
+
+  def test_feed_forward_overflow_check(self):
+    overflow_check_tpl = overflow_check.OverflowLimits(
+        max_val=65500, min_val=-65000
+    )
+    p = pax_fiddle.Config(
+        quantization.FeedForwardOverflowCheck,
+        overflow_limits=overflow_check_tpl,
+        input_dims=8,
+        output_dims=4,
+        name='feed_forward_1',
+    )
+
+    feed_forward_1 = instantiate(p)
+
+    p = pax_fiddle.Config(
+        quantization.FeedForwardOverflowCheck,
+        overflow_limits=overflow_check_tpl,
+        input_dims=8,
+        output_dims=4,
+        name='feed_forward_2',
+    )
+
+    feed_forward_2 = instantiate(p)
+
+    inputs_small = jnp.array(
+        [[1, 2, 3, 4, 5, 6, 7, 8], [9, 10, 11, 12, 13, 14, 15, 16]],
+        dtype=p.dtype,
+    )
+    inputs_large = (
+        jnp.array(
+            [[1, 2, 3, 4, 5, 6, 7, 8], [9, 10, 11, 12, 13, 14, 15, 16]],
+            dtype=p.dtype,
+        )
+        * 100_000
+    )
+
+    with self.assertLogs(level='WARNING') as log_output:
+      with base_layer.JaxContext.new_context():
+        prng_key = jax.random.PRNGKey(seed=123)
+        initial_vars = feed_forward_1.init(prng_key, inputs_small)
+        _ = feed_forward_1.apply(initial_vars, inputs_small)
+        _ = feed_forward_1.apply(initial_vars, inputs_large)
+        _ = feed_forward_2.apply(initial_vars, inputs_small)
+        _ = feed_forward_2.apply(initial_vars, inputs_large)
+
+    self.assertLen(log_output[0], 2)
+    self.assertStartsWith(
+        log_output[0][0].msg, 'Overflow in: FeedForward, feed_forward_1'
+    )
+    self.assertStartsWith(
+        log_output[0][1].msg, 'Overflow in: FeedForward, feed_forward_2'
+    )
+
+  def test_transformer_overflow_check(self):
+    p = pax_fiddle.Config(
+        layers.transformers.Transformer,
+        name='jax_transformer_layer',
+        input_dims=8,
+        hidden_dims=4,
+        num_heads=8,
+    )
+    p.tr_atten_tpl.combine_qkv = True
+    p.tr_fflayer_tpl.use_gated_activation = True
+
+    overflow_check_tpl = overflow_check.OverflowLimits(max_val=-1, min_val=1)
+    feed_forward = pax_fiddle.Config(
+        quantization.FeedForwardOverflowCheck,
+        overflow_limits=overflow_check_tpl,
+    )
+    p.tr_fflayer_tpl.fflayer_tpl = feed_forward
+
+    proj_tpl = pax_fiddle.Config(
+        quantization.AttentionProjectionOverflowCheck,
+        overflow_limits=overflow_check_tpl,
+    )
+    p.tr_atten_tpl.proj_tpl = proj_tpl
+
+    combined_qkv_proj_tpl = pax_fiddle.Config(
+        quantization.CombinedQKVProjectionLayerOverflowCheck,
+        overflow_limits=overflow_check_tpl,
+    )
+    p.tr_atten_tpl.combined_qkv_proj_tpl = combined_qkv_proj_tpl
+
+    transformer = instantiate(p)
+
+    inputs = jnp.expand_dims(
+        jnp.array(
+            [[1, 2, 3, 4, 5, 6, 7, 8]],
+            dtype=p.dtype,
+        ),
+        -1,
+    )
+    paddings = jnp.array(
+        [[0] * 8],
+        dtype=p.dtype,
+    )
+    atten_mask = jnp.expand_dims(
+        jnp.expand_dims(jnp.array([[False] * 8], dtype=p.dtype), 0), 0
+    )
+
+    with base_layer.JaxContext.new_context():
+      prng_key = jax.random.PRNGKey(seed=123)
+      initial_vars = transformer.init(prng_key, inputs, paddings, atten_mask)
+      with self.assertLogs(level='WARNING') as log_output:
+        _ = transformer.apply(initial_vars, inputs, paddings, atten_mask)
+
+    self.assertLen(log_output[0], 7)
+    self.assertStartsWith(
+        log_output[0][0].msg,
+        'Overflow in: CombinedQKVProjection, q, combined_qkv',
+    )
+    self.assertStartsWith(
+        log_output[0][1].msg,
+        'Overflow in: CombinedQKVProjection, k, combined_qkv',
+    )
+    self.assertStartsWith(
+        log_output[0][2].msg,
+        'Overflow in: CombinedQKVProjection, v, combined_qkv',
+    )
+    self.assertStartsWith(
+        log_output[0][3].msg, 'Overflow in: AttentionProjection, post'
+    )
+    self.assertStartsWith(
+        log_output[0][4].msg, 'Overflow in: FeedForward, ffn_layer1_gate'
+    )
+    self.assertStartsWith(
+        log_output[0][5].msg, 'Overflow in: FeedForward, ffn_layer1'
+    )
+    self.assertStartsWith(
+        log_output[0][6].msg, 'Overflow in: FeedForward, ffn_layer2'
+    )
+
+
+if __name__ == '__main__':
+  absltest.main()
